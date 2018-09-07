c6a676bef381ed6e50184686168b5bff3d18f291
Dave Lee <davelee.com@gmail.com>
2018-09-06 21:40:26 -0700

    Add install script

---
+++ install.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+setopt extendedglob
+autoload zmv
+
+readonly installer=$0:P
+
+# noglob to pass * through to zmv
+# -W to make zmv handle * -> .*
+# -L -s does ln -s instead of mv
+# $PWD/* to get symlink with full path
+# ~$installer to exclude this script
+noglob zmv ${DRY_RUN+-n} -W -L -s $PWD/*~$installer ~/.*
